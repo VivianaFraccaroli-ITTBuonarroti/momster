d034bd3bdde6779103a630c1aade51cacf58e990
VivianaFraccaroli-ITTBuonarroti <viviana.fraccaroli@buonarroti.tn.it>
2022-11-15 23:33:01 +0100

    feat: implementazione metodi

--- src/monster.java
+++ src/monster.java
@@ -1,3 +1,21 @@
 public class Monster {
-    
+    private String nome;
+    private int puntiFerita;
+    private int danno;
+
+    void Monster(String nome, int puntiFerita, int danno){
+        this.vita = vita;
+    }
+
+    public int attacca(){
+        return danno;
+    }
+
+    public subisci(int dannoAvversario){
+        puntiFerita-= dannoAvversario;
+    }
+
+    public String toString(){
+        return ("Il mostro " + nome + " che fa " + danno + " punti vita di danno ha ancora " + " di vita")
+    }
 }

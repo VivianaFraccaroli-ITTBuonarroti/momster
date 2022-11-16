a10648b8cedea2abf9195aef9162c89579eeff5b
VivianaFraccaroli-ITTBuonarroti <viviana.fraccaroli@buonarroti.tn.it>
2022-11-16 14:55:53 +0100

    fix: intellij va grazie andreolli

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,3 @@
-package com.company;
-import java.util.Scanner;
-import java.util.Random;
-
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
